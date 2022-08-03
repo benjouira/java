4f1cf10d0a67fcbe83c8bf4bc6a764a96f692bca
Rabii <rabi3jouira@gmail.com>
2022-08-03 22:22:21 +0100

    Add files via upload

---
+++ inheritence/Dog.java
@@ -0,0 +1,13 @@
+package inheritence;
+
+public class Dog extends Animal{
+	
+	private void bark() {
+		System.out.println("Dog '"+ getId() +"'is barking");
+	}
+	
+	@Override
+	public void sound() {
+		bark();
+	}
+}
---
+++ inheritence/MainAnimal.java
@@ -0,0 +1,9 @@
+package inheritence;
+
+public class MainAnimal {
+	public static void main(String[] args) {
+		Animal dog = new Dog();
+		dog.setId(123);
+		dog.sound();
+	}
+}
